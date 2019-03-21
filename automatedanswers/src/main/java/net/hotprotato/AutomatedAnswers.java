4b02119d1efadc92d7a05ec8001b919ccb6db8d1
Jake Peace <jakepeace872@hotmail.com>
2019-03-22 02:56:02 +1100

    Mavonized project

---
+++ automatedanswers/src/main/java/net/hotprotato/AutomatedAnswers.java
@@ -0,0 +1,14 @@
+package net.hotprotato;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AutomatedAnswers extends JavaPlugin {
+
+    public void onEnable(){
+
+    }
+
+    public void onDisable(){
+
+    }
+}
